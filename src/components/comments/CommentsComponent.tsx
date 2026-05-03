f42f949dd2cc0d73cfec422c43c6e34bd072d063
Lanovska-Sofiia10 <lanovskasofia@gmail.com>
2026-05-03 17:02:52 +0300

    corrected task

--- src/components/comments/CommentsComponent.tsx
+++ src/components/comments/CommentsComponent.tsx
@@ -1,14 +1,11 @@
-import {useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const CommentsConponent = ()=> {
-    const navigate = useNavigate();
-    const handleOnClickPlaceHolder = () =>{
-        navigate('jsonplaceholder')
-    }
+
 
     return (
         <div>
-            <button onClick={handleOnClickPlaceHolder}>json place holder</button>
+            <p><Link to="jsonplaceholder">json place holder</Link></p>
         </div>
     );
 };
